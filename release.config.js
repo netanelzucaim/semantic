603756f1a1e3259cb154da8521ec9ca0b77844a8
Your Name <you@example.com>
2026-02-12 23:02:23 +0200

    feat(adding semantic)

--- release.config.js
+++ release.config.js
@@ -6,26 +6,38 @@ module.exports = {
   // Creates tags like: ansible-v1.0.1
   tagFormat: `${pluginName}-v\${version}`,
   plugins: [
-    ["@semantic-release/commit-analyzer", {
-      "preset": "angular",
-      // Only releases if the commit scope matches the plugin folder name
-      "releaseRules": [
-        { "scope": pluginName, "release": "patch" },
-        { "scope": pluginName, "type": "feat", "release": "minor" }
-      ]
-    }],
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        // Only releases if the commit scope matches the plugin folder name
+        releaseRules: [
+          { scope: pluginName, release: "patch" },
+          { scope: pluginName, type: "feat", release: "minor" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/changelog", {
-      "changelogFile": "README.md" // Updates the specific plugin's README
-    }],
-    ["@semantic-release/exec", {
-      // Injects the new version into your Docker build command
-      "prepareCmd": `docker build -t artifactory.com/${pluginName}:\${nextRelease.version} .`,
-      "publishCmd": `docker push artifactory.com/${pluginName}:\${nextRelease.version}`
-    }],
-    ["@semantic-release/git", {
-      "assets": ["README.md", "package.json"],
-      "message": `chore(${pluginName}): release \${nextRelease.version} [skip ci]`
-    }]
-  ]
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "README.md", // Updates the specific plugin's README
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        // Injects the new version into your Docker build command
+        prepareCmd: `docker build -t \${process.env.DOCKER_REG_USERNAME}/${pluginName}:\${nextRelease.version} .`,
+        publishCmd: `docker push \${process.env.DOCKER_REG_USERNAME}/${pluginName}:\${nextRelease.version}`,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["README.md", "package.json"],
+        message: `chore(${pluginName}): release \${nextRelease.version} [skip ci]`,
+      },
+    ],
+  ],
 };
