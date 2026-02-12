ffd8d463b62e09df1c544ae9ee884228614f4366
Your Name <you@example.com>
2026-02-12 23:19:30 +0200

    feat(adding semantic)

--- release.config.js
+++ release.config.js
@@ -28,7 +28,7 @@ module.exports = {
       "@semantic-release/exec",
       {
         // Updates plugin version and builds a versioned image
-        prepareCmd: `npm version --no-git-tag-version \${nextRelease.version} && docker build -t \${process.env.DOCKER_REG_USERNAME}/${pluginName}:\${nextRelease.version} .`,
+        prepareCmd: `npm pkg set version=\${nextRelease.version} && docker build -t \${process.env.DOCKER_REG_USERNAME}/${pluginName}:\${nextRelease.version} .`,
         publishCmd: `docker push \${process.env.DOCKER_REG_USERNAME}/${pluginName}:\${nextRelease.version}`,
       },
     ],
